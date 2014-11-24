f2b066b58f95e6a52a9de3a3703089b123666f13
CSAP1415 <phxbains@yahoo.com>
2014-11-23 17:18:32 -0700

    Update Line.java

--- src/Line.java
+++ src/Line.java
@@ -1,1 +1,40 @@
 // A Line object represents a line defined by a pair of Points.
+//Ajeet Bains//
+import java.awt.Point;
+
+public class Line
+{
+  Point pointStart;
+  Point pointEnd;
+
+  public Line()
+  {
+    pointStart = new Point();
+    pointEnd = new Point();
+  }
+  public Line(Point a, Point b)
+  {
+    pointStart = a;
+    pointEnd = b;
+  }
+  public double length()
+  {
+    return pointStart.distance(pointEnd);
+  }
+  public double slope()
+  {
+    return (pointEnd.y - pointStart.y)/ (pointEnd.x - pointStart.x);
+  }
+ public String toString()
+ {
+   return "(" + pointStart.x + "," + pointStart.y + ")" + " " + "(" + pointEnd.x + "," + pointEnd.y + ")";
+ }
+public void translate(int c, int d)
+{
+  pointStart.translate( c,  d);
+  pointEnd.translate( c,  d);
+}
+}
+
+
+
